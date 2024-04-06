6e87d3c5f8b5ec25dc96ff90207791c0bbb46614
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 21:17:26 -0300

    bumerangizinho

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double PercentualDeBonus, PercentualDeRetorno;
+double pontosATransferir, pontosAReduzir, pontosAAcrescentar;
+
+Console.Clear();
+Console.WriteLine("--- Milhas Bumerangue --- \n");
+
+Console.Write("Parcentual de bonus.....");
+PercentualDeBonus = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("PercentualDeBonus.....:");
+PercentualDeRetorno = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Pontos a transferir.....:");
+pontosATransferir = Convert.ToDouble(Console.ReadLine());
+
+pontosAReduzir = pontosATransferir - (pontosATransferir * PercentualDeRetorno) / 100;
+pontosAAcrescentar = pontosATransferir - (pontosATransferir * PercentualDeRetorno) / 100;
+
+Console.WriteLine();
+Console.WriteLine($"Pontos a reduzir na origem........{pontosAReduzir}:");
+Console.WriteLine($"Pontos a acrescentar no destino...{pontosAAcrescentar}:");
